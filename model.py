88292eb06353e7d8672b6d85ed752d8ed951947d
Landon Smith <landjbs@gmail.com>
2019-09-17 16:48:44 -0400

    initial_score_vec init

--- model.py
+++ model.py
@@ -3,4 +3,6 @@ import keras
 
 def build_model(Language):
     """ Returns structure of model for word vector optimization """
-    inputs = keras.layers.Input(shape-=(None, Language.vocabSize))
+    initial_score_vec = keras.layers.Input(shape=(None, Language.vocabSize),
+                                            name='initial_score_vec')
+    dense_1 =

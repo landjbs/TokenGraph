0ba861b416f1f4abe273f7ee179f3a6c62db9dbd
Landon Smith <landjbs@gmail.com>
2019-09-24 17:13:13 -0400

    TokenGraph str

--- tokengraph.py
+++ tokengraph.py
@@ -7,6 +7,20 @@ import numpy as np
 
 class TokenGraph(object):
     """ Stores all methods for building and accessing token relationships """
-    def __init__(self):
+    # base methods
+    def __init__(self, tokenizer):
+        assert isinstance(tokenizer, Tokenizer), ('tokenizer expected type ' \
+                                                'Tokenizer, but found type ' \
+                                                f'{type(tokenizer)}.')
+        self.tokenizer = tokenizer
         self.corrMatrix = None
-        self.descriptionMatrix = None
+
+    def __str__(self):
+        return f'<TokenGraph Object: TOKENIZER={self.tokenizer}>'
+
+    # save/load methods
+    # TODO: IMPLEMENT
+
+    # matrix initialization methods
+    def build_corr_matrix_from_iterator(self, iterator):
+        """ Builds corr matrix from file iterator """

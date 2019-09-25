92b6b39fc4d910ff6777a2dfd8fde3c15791929f
Landon Smith <landjbs@gmail.com>
2019-09-25 17:08:04 -0400

    utils formatting

--- utils.py
+++ utils.py
@@ -1,17 +1,20 @@
 import os
 import pickle
 
+
 def save_obj(obj, name):
     """ Pickles objectt to file under name """
     with open(f'{name}.sav', 'wb+') as attributeFile:
         pickle.dump(obj, attributeFile)
     return True
 
+
 def load_obj(name):
     """ Loads object from file under name """
     with open(f'{name}.sav', 'rb') as loadFile:
         obj = pickle.load(loadFile)
     return obj
+
 
 def delete_folder(folderPath):
     """ Deletes folderPath and contents """

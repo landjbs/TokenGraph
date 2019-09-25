5c77736fcffe89a1a65283a3a069fc58c919981c
Landon Smith <landjbs@gmail.com>
2019-09-25 17:07:43 -0400

    utils folder manipulation imports

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 
 def save_obj(obj, name):
@@ -11,3 +12,36 @@ def load_obj(name):
     with open(f'{name}.sav', 'rb') as loadFile:
         obj = pickle.load(loadFile)
     return obj
+
+def delete_folder(folderPath):
+    """ Deletes folderPath and contents """
+    if os.path.exists(folderPath):
+        for file in os.listdir(folderPath):
+            os.remove(f'{folderPath}/{file}')
+        os.rmdir(folderPath)
+        return True
+    else:
+        return False
+
+
+def delete_and_make_folder(folderPath):
+    """ Deletes folder if already exists and makes folder """
+    delete_folder(folderPath)
+    os.mkdir(folderPath)
+
+
+def safe_make_folder(folderPath):
+    """ Wraps delete_and_make_folder but checks with the user first """
+    if os.path.exists(folderPath):
+        deleteAction = input(f"""{folderPath} already exists.
+                                Are you sure you want to delete it? (y/n): """)
+        if (deleteAction == 'y'):
+            delete_and_make_folder(folderPath)
+            return True
+        elif (deleteAction == 'n'):
+            raise SaverError('Folder deletion safely cancelled.')
+        else:
+            print("Must input either 'y' or 'n'.")
+            safe_make_folder(folderPath)
+    else:
+        os.mkdir(folderPath)

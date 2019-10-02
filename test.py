fbcbfc4660cfa3460949fce48ee825b2a8bd1c31
Landon Smith <landjbs@gmail.com>
2019-10-02 18:23:23 -0400

    sort methods 1 and 2 imp

--- test.py
+++ test.py
@@ -27,3 +27,47 @@
 #     for id, score in topTokens:
 #         word = graphObj.tokenizer.reverseIdx[id]
 #         print(f'<{score}> {word}')
+
+
+import numpy as np
+from time import time
+
+def make_big():
+    return np.random.randint(0, 10000, size=100000)
+
+def sort_1(l, n):
+    newList = []
+    for i in range(n):
+         minLoc = l.index(min(l))
+         newAdd = l.pop(minLoc)
+         newList += newAdd
+     return newList
+
+ def sort_2(l, n):
+     newList = l[:n]
+     maxElt = max(newList)
+     maxLoc = newList.index(maxElt)
+     for elt in l:
+         if elt < maxElt:
+             _ = newList.pop(maxLoc)
+             newList.append(elt)
+             maxElt = max(newList)
+             maxLoc = newList.index(maxElt)
+     newList.sort()
+     return newList
+
+
+t_1 = 0
+t_2 = 0
+n = 50
+
+from tqdm import tqdm
+
+for i in tqdm(range(100)):
+    x = make_big()
+    # 1
+    s_1 = time()
+    l_1 = sort(x, n)
+    t_1 += time() - s_1
+    # 2
+    
